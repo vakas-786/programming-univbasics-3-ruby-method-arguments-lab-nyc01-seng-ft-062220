376af5da057580aee70ec834a71d2ddbb71b1729
Vakas  Akhtar <vakhtar@fordham.edu>
2020-06-07 22:29:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language(name,language= Ruby)
+def introduction_with_language(name,language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
